1d36e86d5a971c6dc910e95910f7135398cf195f
bvth <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>
2022-07-05 23:13:06 +0300

    use flex for responsiveness

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -4,6 +4,8 @@ import Game from './game';
 export default function Category(props: CategoryModel) {
     return <section className='category'>
         <h2>{props.title}</h2>
-        {props.items.map(item => <Game key={props.title+item.title} {...item}/>)}
+        <div className="flex-container">
+            {props.items.map(item => <Game key={props.title+item.title} {...item}/>)}
+        </div>
     </section>
 }
